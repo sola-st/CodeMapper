cbb64bb29b14c059365a5621641ec74753656588
Hhyemin <huhuimin236@gmail.com>
2024-04-15 14:57:59 +0000

    Update to get more detials about the changeTypes.

--- src/anything_tracker/collect/data_preprocessor/AnalyzeChangeTypes.py
+++ src/anything_tracker/collect/data_preprocessor/AnalyzeChangeTypes.py
@@ -5,23 +5,35 @@ from os.path import join
 from anything_tracker.experiments.SourceRepos import SourceRepos
 
 
-def get_unique_change_types(data, change_types:set):
+def get_unique_change_types(data, overall:list, category):
     '''
     Extrack the unique values of changeType. --> to extract the rule to convert.
     '''
-
+    change_types:dict = overall[0]
+    change_types_detail:dict = overall[1]
     change_histories = data["expectedChanges"]
     for h in change_histories:
         change_type = h["changeType"]
+        # overall counting
         if change_type not in change_types.keys():
             change_types.update({change_type: 1})
         else:
             change_types[change_type] += 1
         change_types["all"] += 1
 
-    return change_types
+        # count more details
+        if category not in change_types_detail.keys():
+            change_types_detail.update({category:{}})
+            change_types_detail[category].update({change_type: 1})
+        else:
+            if change_type not in change_types_detail[category].keys():
+                change_types_detail[category].update({change_type: 1})
+            else:
+                change_types_detail[category][change_type] += 1
 
-def recursive_get_json_files(data_folder, repo_parent_folder, change_types):
+    return overall
+
+def recursive_get_json_files(data_folder, repo_parent_folder, overall, category):
     files = os.listdir(data_folder)
     for file in files:
         file_path = os.path.join(data_folder, file)
@@ -33,20 +45,22 @@ def recursive_get_json_files(data_folder, repo_parent_folder, change_types):
             if repo_name == "jgit":
                 # jgit -- remote: Repository not found.
                 continue
-
-            change_types = get_unique_change_types(data, change_types)
+            
+            assert category != None
+            overall = get_unique_change_types(data, overall, category)
 
         elif os.path.isdir(file_path):
             if file != "test" and file != "training":
+                category = file
                 print(file)
-            change_types = recursive_get_json_files(file_path, repo_parent_folder, change_types)
+            overall = recursive_get_json_files(file_path, repo_parent_folder, overall, category)
 
-    return change_types
+    return overall
 
 
 if __name__=="__main__":
     data_folder = join("data", "oracle_code_tracker")
-    result_file = join("data" "results", "analysis_on_codetracker_data", "change_types.json")
+    result_file = join("data", "results", "analysis_on_codetracker_data", "change_types_details.json")
 
     # prepare repositories
     repo_urls_file = join("data", "results", "analysis_on_codetracker_data", "source_repos_java.txt")
@@ -56,8 +70,11 @@ if __name__=="__main__":
     # source_repo_init.checkout_latest_commits()
     print(f"Found {len(repo_dirs)} repositories.")
     
-    change_types = {"all": 0}
-    change_types = recursive_get_json_files(data_folder, repo_folder_java, change_types)
+    change_types_all = {"all": 0}
+    change_types_detail = {}
+    overall = [change_types_all, change_types_detail]
+    category = None
+    change_types = recursive_get_json_files(data_folder, repo_folder_java, overall, category)
     with open(result_file, "w") as ds:
         json.dump(change_types, ds, indent=4, ensure_ascii=False)
     
