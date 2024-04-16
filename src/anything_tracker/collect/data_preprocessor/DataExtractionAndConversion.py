8a6556140acdcce71a7f2c03fe203b0bd95f5515
Hhyemin <huhuimin236@gmail.com>
2024-04-16 17:15:53 +0000

    Convert data: get region ranges (WIP)

--- src/anything_tracker/collect/data_preprocessor/DataExtractionAndConversion.py
+++ src/anything_tracker/collect/data_preprocessor/DataExtractionAndConversion.py
@@ -2,40 +2,89 @@ import json
 import os
 from os.path import join
 
+from anything_tracker.collect.data_preprocessor.GetRanges import get_range
+from anything_tracker.experiments.SourceRepos import SourceRepos
 
-def get_region_base_info(element_name_info):
+
+def get_region_base_info(element_name_info, category):
+    '''
+    Format:
+        block: src/main/java/org.apache.commons.io.EndianUtils#read(InputStream)$if(475-477)"
+        class: src/main/java/org.apache.commons.io.(public)CopyUtils(30)
+        variable: src/main/java/com.puppycrawl.tools.checkstyle.Checker#fireErrors(String, SortedSet)$element:LocalizedMessage(387)
+        attribute: src/java/org.apache.commons.io.input.Tailer@(final)(private)end:boolean(70)
+                java/compiler/impl/src/com.intellij.packaging.impl.artifacts.ArtifactBySourceFileFinderImpl@myProject:Project(47)
+    '''
+
+    # only "variable" and "attribute" need perfact 'element'.
+    element = None
+    if category == "block":
+        element = element_name_info.split("#")[-1] # coarse grained result
+    elif category == "class":
+        element = element_name_info.split(".")[-1]
+    elif category == "variable":
+        element = element_name_info.split("$element:")[1].split("(")[0]
+    elif category == "attribute":
+        splits_tmp = element_name_info.split(":")[0]
+        if ")" in splits_tmp:
+            element = splits_tmp.split(")")[1]
+        else:
+            element = splits_tmp.split("@")[1]
+    # else: # method
+    assert element != None
+
     tmp = element_name_info.split(".")[-1].split("(")
-    info = f"({tmp[0]}"
-    line_number = tmp[1].replace(")", "")
-    return info, line_number
+    line_number = tmp[-1].replace(")", "")
+    if "-" in line_number: # special for 'block'
+        start_line_number, end_line_number = line_number.split("-")
+        return element, start_line_number, end_line_number
+    else:
+        return element, line_number
 
-def convert_change_type():
-    '''
-    Rules to convert "changeType" (codetracker) to "change_operation" (AnythingTracker).
-    * The goal to set these rules is to filter out no change histories.
-    * Check if deletions
-        1. to nochange
-        2. to 
 
-    '''
-
-    change_operation = None
-    return change_operation
-
-def write_extracted_json_strings(json_file, to_write):
-    with open(json_file, "w") as ds:
+def write_extracted_json_strings(json_file, to_write, write_mode):
+    with open(json_file, write_mode) as ds:
         json.dump(to_write, ds, indent=4, ensure_ascii=False)
 
 
 class DataExtractionAndConversion():
-    def __init__(self, data_folder, output_folder):
+    def __init__(self, data_folder, output_folder, repo_parent_folder):
         self.data_folder = data_folder
         self.output_folder = output_folder
+        self.repo_parent_folder = repo_parent_folder
+        self.partial_categoris = ["attribute", "variable"]
+        self.partial_categoris_3 = ["attribute", "variable"]
+
+        # unify the keys for the start.
+        # note: the expectedHistories have the smae keys for all the 5 categories.
+        self.key_set = {
+            "attribute": {
+                "to_split": "attributeKey",
+                "start_line_number": "attributeDeclarationLine"
+            },
+            "class": {
+                # "to_split": "classKey",
+                "start_line_number": "classDeclarationLine"
+            },
+            # "method" : {
+            #     "to_split": "functionKey",
+            #     "start_line_number": "functionStartLine"
+            # },
+            "variable": {
+                "to_split": "variableKey",
+                "start_line_number": "variableStartLine"
+            },
+            "block" : {
+                "to_split": "blockKey",
+                "start_line_number": "blockStartLine",
+                "end_line_number": "blockEndLine"
+            }
+        }
 
         # change types from the original dataset
-        self.change_types_equals_to_nochange = []
+        # self.change_types_equals_to_nochange = []
         
-    def convert_data(self, json_file, category):
+    def convert_data(self, file_number, json_file, category):
         '''
         Convert data to extract what AnythingTracker needs.
         '''
@@ -45,59 +94,126 @@ class DataExtractionAndConversion():
         
         start_file_path = data["filePath"]
         change_histories = data["expectedChanges"]
-        start_line_number = data["attributeDeclarationLine"]
+        start_line_number = data[self.key_set[category]["start_line_number"]]
         # 1. input used to start tracking. 
         # Always track back, always end with "introduce" - eg., introduce a new variable.
+        source_commit = data["startCommitId"][:8]
+        repo_name = data["repositoryWebURL"].split("/")[-1].replace(".git", "")
+        repo_dir = join(self.repo_parent_folder, repo_name)
+        additional_info=None
+        
+        if category in self.partial_categoris:
+            # key = f"{category}Key"
+            key = self.key_set[category]["to_split"]
+            additional_info, source_line_number = get_region_base_info(data[key], category)
+        elif category == "block":
+            additional_info = data["blockEndLine"]
+        file_path = join(repo_dir, start_file_path)
+        source_range = get_range(repo_dir, source_commit, file_path, start_line_number, additional_info)
         converted_json_str_input = { 
             "url": data["repositoryWebURL"],
             "source_file": start_file_path, 
-            "source_commit": data["startCommitId"],
-            "source_range": f"[{start_line_number}, x, END, y]", 
+            "source_commit": source_commit,
+            "source_range": f"{source_range}", 
             "category": category,
             "time_order": "old to new"
         }
 
-        # 2. expected histories
-        converted_json_str_expect = self.analyze_histories(change_histories)
+        output_dir = join(self.output_folder, category, str(file_number))
+        os.makedirs(output_dir, exist_ok=True)
 
-        file_for_input = join(self.output_folder, category, "source.json")
-        write_extracted_json_strings(file_for_input, converted_json_str_input)
-        file_for_expect = join(self.output_folder, category, "expect.json")
-        write_extracted_json_strings(file_for_expect, converted_json_str_expect)
+        file_for_input = join(output_dir, "source.json")
+        write_extracted_json_strings(file_for_input, converted_json_str_input, "w")
+
+        # 2. expected histories
+        converted_json_str_expect = self.analyze_histories(repo_dir, change_histories, category)
+        
+        # TODO add to write an overall file
+        file_for_expect = join(output_dir, "expect.json")
+        write_extracted_json_strings(file_for_expect, converted_json_str_expect, "w")
             
-    def analyze_histories(self, change_histories):
+    def analyze_histories(self, repo_dir, change_histories, category):
         # Change_histories is a list of json strings, every josn string is a peice of change history
         extracted_change_histories = []
 
         for h in change_histories:
-            if h["changeType"] not in self.change_types_equals_to_nochange:
-                change_opreation = convert_change_type()
-                source_info, source_line_number = get_region_base_info(h["elementNameBefore"])
-                target_info, target_line_number = get_region_base_info(h["elementNameAfter"])
+            # if h["changeType"] not in self.change_types_equals_to_nochange: #TODO
+                # change_opreation = convert_change_type()
+                source_line_number = None
+                target_line_number = None
+                source_additional_info = None
+                target_additional_info = None
+                if category in self.partial_categoris:
+                    # _info shows the source element, like method definition
+                    source_additional_info, source_line_number = get_region_base_info(h["elementNameBefore"], category)
+                    target_additional_info, target_line_number = get_region_base_info(h["elementNameAfter"], category)
+                    source_info = source_additional_info
+                    target_info = target_additional_info
+                elif category == "block":
+                    source_info, source_line_number, source_end_line_number = get_region_base_info(h["elementNameBefore"], category)
+                    target_info, target_line_number, target_end_line_number = get_region_base_info(h["elementNameAfter"], category)
+                    source_additional_info = source_end_line_number
+                    target_additional_info = target_end_line_number
+                elif category == "class":
+                    source_info, source_line_number = get_region_base_info(h["elementNameBefore"], category)
+                    target_info, target_line_number = get_region_base_info(h["elementNameAfter"], category)
+
+                assert source_line_number != None
+                assert target_line_number != None
+
+                source_range = None
+                parent_commit = h["parentCommitId"][:8]
+                if parent_commit != "0":
+                    source_file_path = join(repo_dir, h["elementFileBefore"])
+                    source_range = get_range(repo_dir, parent_commit, source_file_path, source_line_number, source_additional_info)
+                target_file_path = join(repo_dir, h["elementFileAfter"])  
+                target_range = get_range(repo_dir, h["commitId"][:8], target_file_path, target_line_number, target_additional_info)
+
+                if source_range != None: # to handle 'introduce'
+                    source_range_to_json = f"{source_range}"
+                else:
+                    source_range_to_json = source_range
+                    
                 extracted_h = {
                     "source_commit": h["parentCommitId"],
                     "target_commit": h["commitId"],
                     "source_file": h["elementFileBefore"],
                     "target_file": h["elementFileAfter"],
-                    "change_operation": change_opreation,
+                    "change_operation": h["changeType"],
                     "source_info": source_info,
                     "target_info": target_info,
-                    "source_line_number": source_line_number,
-                    "target_line_number": target_line_number,
+                    "source_range": source_range_to_json,
+                    "target_range": f"{target_range}",
+                    "category": category,
                     "comment": h["comment"]
                 }
                 extracted_change_histories.append(extracted_h)
 
         return extracted_change_histories
 
-
-    def recursive_get_json_files(self, category):
-        files = os.listdir(self.data_folder)
-        for file in files:
-            file_path = os.path.join(self.data_folder, file)
+    def recursive_get_json_files(self, data_folder, category):
+        files = os.listdir(data_folder)
+        for i, file in enumerate(files):
+            file_path = os.path.join(data_folder, file)
             if os.path.isfile(file_path):
-                self.convert_data(file_path, category)
+                self.convert_data(i, file_path, category)
             elif os.path.isdir(file_path):
                 if file != "test" and file != "training":
                     category = file
-                self.recursive_get_json_files(file_path, category)
+                if category != "method" and category != "training":
+                    print(category)
+                    self.recursive_get_json_files(file_path, category)
+
+
+if __name__=="__main__":
+    data_folder = join("data", "oracle_code_tracker/attribute")
+    output_folder = join("data", "converted_data")
+    repo_parent_folder = join("data", "repos_java")
+
+    # prepare repositories
+    repo_urls_file = join("data", "results", "analysis_on_codetracker_data", "source_repos_java.txt")
+    source_repo_init = SourceRepos(repo_urls_file, repo_parent_folder)
+    repo_dirs = source_repo_init.get_repo_dirs()
+    # source_repo_init.checkout_latest_commits()
+    print(f"Found {len(repo_dirs)} repositories.")
+    DataExtractionAndConversion(data_folder, output_folder, repo_parent_folder).recursive_get_json_files(data_folder, "attribute")
---
+++ src/anything_tracker/collect/data_preprocessor/GetRanges.py
@@ -0,0 +1,94 @@
+'''
+The original data has 5 categories:
+ * attribute
+ * class
+ * method
+ * variable
+ * block
+
+Ways to get ranges:
+ 1. For variable, and attribute, index them in the provided line. [check if multiple locations exist.]
+ 2. For block, it provides both start and end line number.
+ 3. For class, pair the braces. {}
+ 4. For method, no line numbers in histories. # TODO
+'''
+
+import os
+from git.repo import Repo
+
+
+def get_range(repo_dir, commit, file, start_line_number_str, additional_info):
+    '''
+    additional_info has multiple values:
+     * None
+     * end line number for blocks (int)
+     * element for variables and attributes (str)
+    '''
+    four_element_range = None # to return
+
+    repo = Repo(repo_dir)
+    repo.git.checkout(commit, force=True)
+
+    if not os.path.exists(file):
+        # introduce a new file, the file does not exist in parent commit.
+        return four_element_range
+    
+    with open(file, "r") as f:
+        file_lines = f.readlines()
+    
+    start_line_number = int(start_line_number_str)
+    start_line_number_idx = start_line_number - 1
+    start_line = file_lines[start_line_number_idx]
+
+    if isinstance(additional_info, int): # block
+        start_character_abs = len(start_line) - len(start_line.lstrip()) + 1
+        end_character_abs = len(file_lines[additional_info-1].rstrip)
+        four_element_range = [start_line_number, start_character_abs, additional_info, end_character_abs]
+    elif isinstance(additional_info, str): # variable, attribute
+        intra_line_location_num = start_line.count(additional_info)
+        if intra_line_location_num > 1:
+            print(f"{intra_line_location_num}, {commit}, {file}")
+        if intra_line_location_num == 0: # newly added
+            four_element_range = [start_line_number, 0, start_line_number, 0]
+        else:
+            start_character_abs = start_line.index(additional_info) + 1
+            end_character_abs = start_character_abs + len(additional_info) - 1
+            four_element_range = [start_line_number, start_character_abs, start_line_number, end_character_abs]
+    else: # class, method
+        lines_to_check = file_lines[start_line_number_idx:]
+        file_lines_max = len(file_lines) + 1
+        left_braces_num = 0
+        right_brace_num = 0
+        end_line_number = None
+        end_character_abs = None
+        comment_end = True
+        for i_abs, line in zip(range(start_line_number, file_lines_max), lines_to_check):
+            if comment_end == False:
+                if line.rstrip().endswith("*/"):
+                    comment_end = True
+                else:
+                    continue
+
+            if not line.lstrip().startswith(("//", "/*", "@")): # exclude comment lines, annotations
+                #TODO { or } may just be a char in a string -> yes, it happens
+                if line.rstrip().endswith("{"):
+                # if "{" in line:
+                    left_braces_num += 1
+                if line.rstrip().endswith("}"): # comment follows?
+                # if "}" in line:
+                    right_brace_num += 1
+                if left_braces_num != 0 and left_braces_num == right_brace_num:
+                    end_line_number = i_abs
+                    end_character_abs = len(line)
+                    break
+            elif line.startswith("/*"):
+                # /* --> multi-line comment
+                # /** --> documentation
+                comment_end = False
+
+        assert end_line_number != None
+
+        start_character_abs = len(start_line) - len(start_line.lstrip()) + 1
+        four_element_range = [start_line_number, start_character_abs, end_line_number, end_character_abs]
+
+    return four_element_range # available to return the range characters if needed.
