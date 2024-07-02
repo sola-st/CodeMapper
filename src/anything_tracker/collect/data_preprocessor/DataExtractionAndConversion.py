78ef7fd4e34c2e5fc8b65b6c75b42953675556ce
Hhyemin <huhuimin236@gmail.com>
2024-07-02 11:05:25 +0000

    Remove code for extract source info, and keep the subset: training and test.

--- src/anything_tracker/collect/data_preprocessor/DataExtractionAndConversion.py
+++ src/anything_tracker/collect/data_preprocessor/DataExtractionAndConversion.py
@@ -37,14 +37,11 @@ class DataExtractionAndConversion():
         self.attribute_ast_init = None
         self.variable_ast_init = None
 
-        self.overall_source_json_strs = []
         self.multi_location_infos = [["Folder number", "Multi-location", "Commit", "File path"]]
+        self.write_mode = "w"
 
     def convert_data(self, file_number, json_file, category, subfolder):
-        '''
-        Convert data to extract what AnythingTracker needs.
-        '''
-
+        # Convert data to extract what AnythingTracker needs. 
         with open(json_file) as f:
             data = json.load(f)
         
@@ -54,81 +51,25 @@ class DataExtractionAndConversion():
         # Analyze and write expected histories
         change_histories = data["expectedChanges"]
         repo_url = data["repositoryWebURL"]
-        converted_json_str_expect, extracted_commit_range_pieces, source_info = \
+        converted_json_str_expect, expect_simple = \
                 self.analyze_histories(repo_dir, change_histories, category, repo_url, file_number)
 
-        if subfolder: 
-            output_dir = join(self.output_folder, category, subfolder, str(file_number))
-        else:
-            output_dir = join(self.output_folder, category, str(file_number))
-        os.makedirs(output_dir, exist_ok=True)
-
-        file_for_expect = join(output_dir, "expect_full_histories.json")
-        write_extracted_json_strings(file_for_expect, converted_json_str_expect, "w")
-        file2_for_expect = join(output_dir, "expect_simple.json")
-        write_extracted_json_strings(file2_for_expect, extracted_commit_range_pieces, "w")
-
-        # Write input used to start tracking. 
-        # Always track back, always end with "introduce" - eg., introduce a new variable.
-        start_file_path = data["filePath"]
-        start_line_number = data[self.key_set[category]["start_line_number"]]
-        source_commit = data["startCommitId"]
-        
-        additional_info=None
-        if category in self.partial_categories:
-            additional_info = data[self.key_set[category]["start_name"]]
-        elif category == "block":
-            additional_info = data["blockEndLine"]
-            
-        file_path = join(repo_dir, start_file_path)
-
-        if category in self.group_1:
-            source_range, multi_location_list = GetRanges(repo_dir, source_commit, file_path, start_line_number, additional_info).run()
-        else: # group 2: class, method, attribute and varable
-            repo = Repo(repo_dir)
-            repo.git.checkout(source_commit, force=True)
-            start_name = data[self.key_set[category]["start_name"]]
-            element_key_line = data[self.key_set[category]["start_info"]]
-            source_info = get_region_base_info(element_key_line, category)
-            start_name_copy, identifier = source_info
-            assert start_name == start_name_copy
-            if category == "method":
-                ast_init = self.method_ast_init
-            elif category == "class":
-                ast_init = self.class_ast_init
-            elif category == "attribute":
-                ast_init = self.attribute_ast_init
-            else: 
-                ast_init = self.variable_ast_init
-            source_range = ast_init.parseJavaFile(file_path, start_name, identifier) 
-            
-        if multi_location_list or len(source_range) > 1:
-            if multi_location_list:
-                multi_location_list.insert(0, file_number)
+        if converted_json_str_expect: # equal to if expect_simple, they are always in the same status: exists or not.
+            if subfolder: 
+                output_dir = join(self.output_folder, category, subfolder, str(file_number))
             else:
-                multi_location_list.append([file_number, source_range, source_commit, file_path])
+                output_dir = join(self.output_folder, category, str(file_number))
+            os.makedirs(output_dir, exist_ok=True)
 
-            self.multi_location_infos.append(multi_location_list)
-
-        converted_json_str_input = { 
-            "url":  repo_url,
-            "source_file": start_file_path, 
-            "source_commit": source_commit,
-            "source_range": f"{source_range}", 
-            "source_info": source_info,
-            "category": category
-        }
-
-        # Write an individual source file for faster checking.
-        file_for_input = join(output_dir, "source.json")
-        write_extracted_json_strings(file_for_input, converted_json_str_input, "w")
-
-        return converted_json_str_input
+            file_for_expect = join(output_dir, "expect_full_histories.json")
+            write_extracted_json_strings(file_for_expect, converted_json_str_expect, "w")
+            file2_for_expect = join(output_dir, "expect_simple.json")
+            write_extracted_json_strings(file2_for_expect, expect_simple, "w")
 
     def analyze_histories(self, repo_dir, change_histories, category, repo_url, file_number):
         # Change_histories is a list of json strings, every json string is a piece of change history
         extracted_change_histories = []
-        extracted_commit_range_pieces = {"url":  repo_url}
+        expect_simple = {"url":  repo_url}
 
         for h in change_histories:
             source_line_number = None
@@ -152,7 +93,7 @@ class DataExtractionAndConversion():
                 source_info = source_results
                 target_info = target_results
                 source_range, target_range = self.analyze_histories_group_2(repo_dir, \
-                        h, source_info, target_info, category)
+                        h, source_info, target_info, category, file_number)
             
             if category in self.group_1: # block
                 source_range, target_range = self.analyze_histories_group_1(repo_dir, \
@@ -161,6 +102,8 @@ class DataExtractionAndConversion():
 
             if source_range != None: # to handle 'introduce'
                 source_range_to_json = f"{source_range}"
+            elif source_range == "drop_current_data":
+                continue # multi ranges found, filter out current history
             else:
                 source_range_to_json = source_range
 
@@ -184,19 +127,19 @@ class DataExtractionAndConversion():
                 "target_range": target_range_to_json,
                 "category": category
             }
-            try:
-                extracted_h_full.update({"comment": h["comment"]})
-            except:
-                extracted_h_full.update({"comment": ""})
+            # try:
+            #     extracted_h_full.update({"comment": h["comment"]})
+            # except:
+            #     extracted_h_full.update({"comment": ""})
             extracted_change_histories.append(extracted_h_full)
 
             # get a simple version of the expected region locations.
             extracted_pieces_1 = get_commit_range_pieces(source_commit, source_file, source_range_to_json)
-            extracted_commit_range_pieces.update(extracted_pieces_1)
+            expect_simple.update(extracted_pieces_1)
             extracted_pieces_2 = get_commit_range_pieces(target_commit, target_file, target_range_to_json)
-            extracted_commit_range_pieces.update(extracted_pieces_2)
+            expect_simple.update(extracted_pieces_2)
         
-        return extracted_change_histories, extracted_commit_range_pieces, source_info
+        return extracted_change_histories, expect_simple
     
     def analyze_histories_group_1(self, repo_dir, h, source_line_number, target_line_number, \
             source_additional_info, target_additional_info, file_number):
@@ -223,7 +166,7 @@ class DataExtractionAndConversion():
 
         return source_range , target_range
     
-    def analyze_histories_group_2(self, repo_dir, h, source_reference, target_reference, category):
+    def analyze_histories_group_2(self, repo_dir, h, source_reference, target_reference, category, file_number):
         repo = Repo(repo_dir)
         
         '''
@@ -252,11 +195,20 @@ class DataExtractionAndConversion():
             source_file_path = join(repo_dir, h["elementFileBefore"])
             if os.path.exists(source_file_path):
                 source_range = ast_init.parseJavaFile(source_file_path, source_element_name, source_identifier)
-   
-        repo.git.checkout(h["commitId"], force=True)
+        
+        child_commit = h["commitId"]
+        repo.git.checkout(child_commit, force=True)
         target_file_path = join(repo_dir, h["elementFileAfter"])  
         target_range = ast_init.parseJavaFile(target_file_path, target_element_name, target_identifier)
 
+        multi_location_list = []
+        if len(source_range) > 1:
+            multi_location_list.append(f"{category}_{file_number}", source_range, parent_commit, source_file_path)
+        if len(target_range) > 1:
+            multi_location_list.append(f"{category}_{file_number}", source_range, child_commit, target_file_path)
+        if multi_location_list:
+            self.multi_location_infos.extend(multi_location_list)
+            source_range == target_range == "drop_current_data"
 
         return source_range , target_range
 
@@ -303,19 +255,14 @@ class DataExtractionAndConversion():
                     end_time = time.time()
                     print(f"{category}-{subfolder}: {round((end_time - self.start_time), 3)} seconds.\n")
 
-                    # Write an overall source file to start checking.
-                    if self.overall_source_json_strs:
-                        file_for_overall = join(self.output_folder, f"converted_data_{category}_{subfolder}.json")
-                        write_extracted_json_strings(file_for_overall, self.overall_source_json_strs, "w")
-                        self.overall_source_json_strs = []
-
+                    # all multi-location cases in one file
                     if len(self.multi_location_infos) > 1:
                         file_for_multi_location = join(self.output_folder, f"converted_data_{category}_{subfolder}_multi.csv")
-                        with open(file_for_multi_location, 'w') as f:
+                        with open(file_for_multi_location, self.write_mode) as f:
                             writer = csv.writer(f)
                             writer.writerows(self.multi_location_infos)
                         self.multi_location_infos = [["Folder number", "Multi-location", "Commit", "File path"]]
-                
+                        self.write_mode = "a"
 
     def main(self):
         self.start_time = time.time()
